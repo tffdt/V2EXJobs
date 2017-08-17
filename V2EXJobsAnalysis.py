7614540e5920d02bb6aac784cc75a81a0447ce5a
tffdt <1020616610@qq.com>
2017-08-17 18:01:33 +0800

    Add files via upload

--- V2EXJobsAnalysis.py
+++ V2EXJobsAnalysis.py
@@ -12,12 +12,22 @@ import requests
 from bs4 import BeautifulSoup
 
 multilist = [[0 for col in range(100)] for row in range(2000)]
+filename = time.strftime("%Y_%m_%d_%H_%M_%S") + '.txt'
+file = open(filename,'wb')
+cnt = 0
 
 def show_res():
     pass
 
-def analyse_infofpage(page_inf):
+def analyse_infofpage(page_num):
     pass
+    # try:
+    #     for i in range(1,100):
+    #         seg_list = jieba.cut(multilist[1][1],cut_all=False)
+    #     print (u'/ '.join(seg_list))
+    # except:
+    #     print (u'分析时出错')
+    #     os._exit()
 
 def get_infofpage(page_num):
     try:
@@ -25,17 +35,18 @@ def get_infofpage(page_num):
         r = requests.get(page_url).text
         soupObj = BeautifulSoup(r, 'html.parser')
         jobinf = soupObj.findAll('span')
-        cnt = 0
+
         for span in jobinf:
             spanpattern = re.compile(r'<a href=')
             strspan = str(span.contents)
             searchresult = spanpattern.search(strspan)#
             if searchresult:
-                multilist[page_num][cnt] = span.text
-                cnt = cnt + 1
+                file.write(span.text)
+                # multilist[page_num][cnt] = span.textspan.text
         print (page_num)
     except:
         print(u'获取' + str(page_num) + u'页信息失败')
+        os._exit(0)
 
 def get_numberofpage(main_url):
     try:
@@ -52,6 +63,8 @@ if __name__ == '__main__':
     main_url = 'https://www.v2ex.com/go/jobs'
     page_num = get_numberofpage(main_url)
     print (u'共找到' + str(page_num) + u'页招聘信息')
-    for num in range(1, 2):   # int(page_num)
+    for num in range(1, int(page_num)):   # int(page_num)
         get_infofpage(num)
-        time.sleep(1)
+        time.sleep(2)
+    # analyse_infofpage(1)
+    file.close()
